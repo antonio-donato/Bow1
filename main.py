974bc19145c9c07fe59d100a3fcee1d113631022
Antonio Donato <antonio.donato@cgm.com>
2021-04-06 23:43:29 +0200

    gestione tiro aggiuntivo

--- main.py
+++ main.py
@@ -1,52 +1,81 @@
-import random
+from partita import partita
+from partita import partitaFinale
 
 
-class partita:
-    def __init__(self):
-        self.lanci = [0, 0]
-        self.str_spare = [False, False]  # [0] - Strike; [1] - Spare
 
-    def lancio(self, numero_lancio, restanti=10):
-        indice_lancio = numero_lancio
-        birilli_restanti = restanti
-        self.lanci[indice_lancio] = random.randint(0, birilli_restanti)
-        if self.lanci[indice_lancio] == birilli_restanti:
-            self.str_spare[indice_lancio] = True  # Strike/Spare in corrispondenza del tiro nel game
-        return self.lanci[indice_lancio]
+def double(strike):
+    if strike == 0:
+        return False
+    return True
 
-
-class partitaFinale(partita):
-    def __init__(self):
-        super().__init__()
-        self.lanci = [0, 0, 0]
-        self.str_spare = [False, False, False]
 
 
 if __name__ == '__main__':
-    match = []
+    PARTITA_EXTRA = 9
+    match = []                      # Lista contenente gli Oggetti "partita"
     punteggioTotaleIncrementale = 0
+    ho_fatto_strike = 0             # può assumere il valore di 2 o 1 a seconda di uno STRIKE o di uno SPARE
+    raddoppio = False               # quando la variabile ho_fatto_strike è > 0 raddoppio il punteggio del lancio
 
     for index in range(10):
         pin_restanti = 10
-        tiro = 0
 
         if index != 9:
             match.append(partita())
         else:
             match.append(partitaFinale())
 
-        # print(type(match[index]))
-        tiro = match[index].lancio(0)
+        raddoppio = double(ho_fatto_strike)
+        if raddoppio:
+            ho_fatto_strike -= 1
+
+        tiro = match[index].lancio(0, pin_restanti, raddoppio)
         pin_restanti -= tiro
         # Strike
         if pin_restanti == 0:
-            continue
+            ho_fatto_strike = 2
+            if index != 9:
+                continue
+            else:
+                pin_restanti = 10
 
-        tiro = match[index].lancio(1, pin_restanti)
+        raddoppio = double(ho_fatto_strike)
+        if raddoppio:
+            ho_fatto_strike -= 1
+
+        tiro = match[index].lancio(1, pin_restanti, raddoppio)
+        pin_restanti -= tiro
+
+        if index == 9:
+            if pin_restanti == 0:
+                ho_fatto_strike = 1
+                pin_restanti = 10
+
+            raddoppio = double(ho_fatto_strike)
+            if raddoppio:
+                ho_fatto_strike -= 1
+
+            if ho_fatto_strike > 0:
+                tiro = match[index].lancio(2, pin_restanti, raddoppio)
+
+        else:  # SPARE
+            if pin_restanti == 0:
+                ho_fatto_strike = 1
 
     for index in range(10):
         print(f"Risultati del lancio {index + 1}: \nTiri: {match[index].lanci}")
+        if match[index].extra_point != 0: print(f"Extra punti: {match[index].extra_point}")
         if match[index].str_spare[0]:
             print("Hai fatto Strike!")
         if match[index].str_spare[1]:
-            print("Hai fatto Spare!")
+            if index != 9:
+                print("Hai fatto Spare!")
+                continue
+            elif match[index].str_spare[0]:
+                print("Hai fatto Strike!")
+            else:
+                print("Hai fatto Spare!")
+
+        if index == 9:
+            if match[index].str_spare[2]:
+                print("Hai fatto Strike!")
---
+++ partita.py
@@ -0,0 +1,26 @@
+import random
+
+class partita:
+    def __init__(self):
+        self.lanci = [0, 0]
+        self.extra_point = 0
+        self.str_spare = [False, False]  # [0] - Strike; [1] - Spare
+
+    def lancio(self, numero_lancio, restanti=10, raddoppio=False): # Se nei precedenti lanci ho fatto strike o spare, accantono il valore del piunteggio in extra point
+        indice_lancio = numero_lancio
+        birilli_restanti = restanti
+        birilli = random.randint(0, birilli_restanti)
+        self.lanci[indice_lancio] = birilli
+        if self.lanci[indice_lancio] == birilli_restanti:
+            self.str_spare[indice_lancio] = True  # Strike/Spare in corrispondenza del tiro nel game
+        if raddoppio:
+            self.extra_point += birilli
+        return self.lanci[indice_lancio]
+
+
+class partitaFinale(partita):
+    def __init__(self):
+        super().__init__()
+        self.lanci = [0, 0, 0]
+        self.str_spare = [False, False, False]
+
