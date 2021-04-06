d45e37ea79cbe4ca80cbf9a4a709cbe2eef1aa6b
Antonio <donant@gmail.com>
2021-04-06 16:41:32 +0200

    refactoring sul lancio

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
 import random
 
 
-class partita():
+class partita:
     def __init__(self):
         self.lanci = [0, 0]
         self.str_spare = [False, False]  # [0] - Strike; [1] - Spare
 
-    def lancio(self, remain_pins=10):
-        self.remain_pins = remain_pins
-        tiro = random.randint(1, self.remain_pins)
-        return tiro
-
-    def lancio1(self, restanti=10):
-        self.restanti = restanti
-        self.lanci[0] = self.lancio(self.restanti)
-        if self.lanci[0] == self.restanti:
-            self.str_spare[0] = True  # Strike
-        return self.lanci[0]
-
-    def lancio2(self, restanti=10):
-        self.restanti = restanti
-        self.lanci[1] = self.lancio(self.restanti)
-        if self.lanci[1] == self.restanti:
-            self.str_spare[1] = True  # Spare
-        return self.lanci[1]
+    def lancio(self, numero_lancio, restanti=10):
+        indice_lancio = numero_lancio
+        birilli_restanti = restanti
+        self.lanci[indice_lancio] = random.randint(0, birilli_restanti)
+        if self.lanci[indice_lancio] == birilli_restanti:
+            self.str_spare[indice_lancio] = True  # Strike/Spare in corrispondenza del tiro nel game
+        return self.lanci[indice_lancio]
 
 
 class partitaFinale(partita):
@@ -31,18 +20,6 @@ class partitaFinale(partita):
         super().__init__()
         self.lanci = [0, 0, 0]
         self.str_spare = [False, False, False]
-
-    def lancio3(self, restanti=10):
-        self.restanti = restanti
-        self.lanci[2] = self.lancio(self.restanti)
-        if self.lanci[2] == self.restanti:
-            self.str_spare[2] = True  #......
-        return self.lanci[2]
-
-
-def randomizza():
-    tiro = random.randint(1, 10)
-    print(tiro)
 
 
 if __name__ == '__main__':
@@ -59,13 +36,13 @@ if __name__ == '__main__':
             match.append(partitaFinale())
 
         # print(type(match[index]))
-        tiro = match[index].lancio1()
+        tiro = match[index].lancio(0)
         pin_restanti -= tiro
         # Strike
         if pin_restanti == 0:
             continue
 
-        tiro = match[index].lancio2(pin_restanti)
+        tiro = match[index].lancio(1, pin_restanti)
 
     for index in range(10):
         print(f"Risultati del lancio {index + 1}: \nTiri: {match[index].lanci}")
